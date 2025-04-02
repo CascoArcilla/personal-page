11852512b3a737cbbc5b62a21ad1b55ac17da127
CascoArcilla <normon1230@gmail.com>
2025-04-02 09:55:55 -0600

    First Uploand files

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+let m = document.querySelector("a");
+console.log(m);
+
+// m.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     window.moveTo(500, 0);
+// });
+
+/**
+ *
+ * @param {*} e
+ */
+function moveToPlus(e) {}
