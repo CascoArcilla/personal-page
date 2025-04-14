78aa657336142ad2bca52b8453bf6209f188bb77
CascoArcilla <normon1230@gmail.com>
2025-04-14 11:48:12 -0600

    Make uml section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-let m = document.querySelector("a");
-console.log(m);
+let erLi = document.getElementsByClassName("e-r")[0];
+let classLi = document.getElementsByClassName("class")[0];
+let cuLi = document.getElementsByClassName("c-u")[0];
 
-// m.addEventListener("click", (e) => {
-//     e.preventDefault()
-//     window.moveTo(500, 0);
-// });
+function showUmlIcon() {
+  let aside = document.getElementsByClassName("target-uml")[0];
+  aside.style.backgroundImage = "url('../assets/imgs/uml.png')";
+}
 
-/**
- *
- * @param {*} e
- */
-function moveToPlus(e) {}
+function showERDiagram() {
+  let aside = document.getElementsByClassName("target-uml")[0];
+  aside.style.backgroundImage = "url('../assets/imgs/ER.jpg')";
+}
+
+function showClassDiagram() {
+  let aside = document.getElementsByClassName("target-uml")[0];
+  aside.style.backgroundImage = "url('../assets/imgs/class.jpg')";
+}
+
+function showCuDiagram() {
+  let aside = document.getElementsByClassName("target-uml")[0];
+  aside.style.backgroundImage = "url('../assets/imgs/cu.webp')";
+}
+
+erLi.addEventListener("mouseenter", showERDiagram);
+erLi.addEventListener("mouseleave", showUmlIcon);
+
+classLi.addEventListener("mouseenter", showClassDiagram);
+classLi.addEventListener("mouseleave", showUmlIcon);
+
+cuLi.addEventListener("mouseenter", showCuDiagram);
+cuLi.addEventListener("mouseleave", showUmlIcon);
