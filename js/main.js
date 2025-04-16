7ad938eee21f41c7aef2b8414f3dd3b59379a0a1
CascoArcilla <normon1230@gmail.com>
2025-04-15 19:47:44 -0600

    Move exact section position

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+/* Move to exact point of secction by id in page */
+function movePageTo(idSecction) {
+  let heightNav = document.getElementsByClassName("nav-ul")[0].clientHeight;
+  let marginTopTargetPostion = 20;
+
+  if (idSecction == "inicio") {
+    scrollTo({ top: 0 });
+    return;
+  }
+
+  let positionY = document
+    .getElementById(idSecction)
+    .getBoundingClientRect().top;
+  let targetPosition = positionY - heightNav - marginTopTargetPostion;
+
+  scrollBy({ top: targetPosition });
+}
+
+/* Change icon  diagram */
 let erLi = document.getElementsByClassName("e-r")[0];
 let classLi = document.getElementsByClassName("class")[0];
 let cuLi = document.getElementsByClassName("c-u")[0];
