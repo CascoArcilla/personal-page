90ccdd58a3f3af9965d600cde2e57a019200c157
CascoArcilla <normon1230@gmail.com>
2025-04-16 10:55:10 -0600

    Diagrams change interval

--- js/main.js
+++ js/main.js
@@ -15,37 +15,3 @@ function movePageTo(idSecction) {
 
   scrollBy({ top: targetPosition });
 }
-
-/* Change icon  diagram */
-let erLi = document.getElementsByClassName("e-r")[0];
-let classLi = document.getElementsByClassName("class")[0];
-let cuLi = document.getElementsByClassName("c-u")[0];
-
-function showUmlIcon() {
-  let aside = document.getElementsByClassName("target-uml")[0];
-  aside.style.backgroundImage = "url('../assets/imgs/uml.png')";
-}
-
-function showERDiagram() {
-  let aside = document.getElementsByClassName("target-uml")[0];
-  aside.style.backgroundImage = "url('../assets/imgs/ER.jpg')";
-}
-
-function showClassDiagram() {
-  let aside = document.getElementsByClassName("target-uml")[0];
-  aside.style.backgroundImage = "url('../assets/imgs/class.jpg')";
-}
-
-function showCuDiagram() {
-  let aside = document.getElementsByClassName("target-uml")[0];
-  aside.style.backgroundImage = "url('../assets/imgs/cu.webp')";
-}
-
-erLi.addEventListener("mouseenter", showERDiagram);
-erLi.addEventListener("mouseleave", showUmlIcon);
-
-classLi.addEventListener("mouseenter", showClassDiagram);
-classLi.addEventListener("mouseleave", showUmlIcon);
-
-cuLi.addEventListener("mouseenter", showCuDiagram);
-cuLi.addEventListener("mouseleave", showUmlIcon);
